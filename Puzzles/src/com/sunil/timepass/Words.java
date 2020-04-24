c2dd9a768ee9992411e018194d5da2121284a9b5
sbhaskar <sbhaskar@adobe.com>
2020-04-24 23:00:42 +0530

    cleaned

--- Puzzles/src/com/sunil/timepass/Words.java
+++ Puzzles/src/com/sunil/timepass/Words.java
@@ -5,11 +5,6 @@ public class Words {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
-=======
-		
-		
->>>>>>> branch 'Sunil' of https://github.com/sunil-bhaskaran/puzzles.git
 	}
 
 }
